224c1e7081dd72589c1c7fe611d02d332c161816
Nic-Gould <108347499+Nic-Gould@users.noreply.github.com>
2023-06-22 12:14:11 +1000

    Update yolo.rs
    fixed missing newline

--- src/yolo.rs
+++ src/yolo.rs
@@ -70,7 +70,8 @@ impl YOLO {
 
         let pred = self
             .model
-            .forward_ts(&[img]).unwrap()
+            .forward_ts(&[img])
+            .unwrap()
             .to_device(tch::Device::Cpu);
         let result = self.non_max_suppression(&pred.get(0), conf_thresh, iou_thresh);
 
